772d79a7733c4d3006f649a4ff68633ceb253069
Ruotsalainen Ville Veikka Matias <ville.v.ruotsalainen@student.jyu.fi>
2024-10-19 16:11:42 +0300

    Lisattiin maa, hahmo ja hahmon liike oikealle

--- Seikkailu Pohjois-savossa/Seikkailu Pohjois-savossa.cs
+++ Seikkailu Pohjois-savossa/Seikkailu Pohjois-savossa.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 using Jypeli;
 using Jypeli.Assets;
 using Jypeli.Controls;
@@ -14,30 +15,57 @@ namespace Seikkailu_Pohjois_savossa;
 /// </summary>
 public class Seikkailu_Pohjois_savossa : PhysicsGame
 {
-    static Image hahmonkuva = LoadImage( "hahmo.png" );
-    
+    private readonly Vector liikeVasemmalle = new Vector(-100, 0);
+    private readonly Vector liikeOikealle = new Vector(100, 0);
     public override void Begin()
     {
         // Kirjoita ohjelmakoodisi tähän
-
+        Camera.ZoomToLevel(20);
         LuoKentta();
-       
+        PhysicsObject hahmo = LisaaHahmo(this, Level.Left + 100, Level.Bottom + 100);
+
+        HahmonOhjaus(hahmo);
+        
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
 
     void LuoKentta()
     {
-        PhysicsObject hahmo = HahmoCollision(this, 100, 200);
-        hahmo.Image = hahmonkuva;
-        Add(hahmo);
+        Surface alareuna = Surface.CreateBottom(Level);
+        alareuna.Position = new Vector(0, Level.Bottom);
+        Add(alareuna);
     }
-
-
-    public static PhysicsObject HahmoCollision(PhysicsGame peli, int width, int height)
+    public static PhysicsObject LisaaHahmo(PhysicsGame peli, double x, double y)
     {
-        PhysicsObject hahmo = PhysicsObject.CreateStaticObject(20.0, 100.0);
+        PhysicsObject hahmo = new PhysicsObject(71, 226);
         hahmo.Shape = Shape.Rectangle;
+        hahmo.Position = new Vector(x, y);
+        hahmo.Restitution = 0;
+        hahmo.CanRotate = false; //Pidetaan hahmo pystyssa
+        hahmo.Tag = "hahmo";
+        Image hahmonkuva = LoadImage("hahmo"); //Pelaajan kuva
+        hahmo.Image = hahmonkuva; // Laitetaan kuva linnusta pallon päälle
+        hahmo.MaxVelocity = 500;  // Linnun maksiminopeus
+        peli.Add(hahmo);
         return hahmo;
     }
+    
+    
+    private void HahmonOhjaus(PhysicsObject hahmo)
+    {
+        Keyboard.Listen(Key.A, ButtonState.Down, Liiku, "Hahmo liikkuu vasemmalle", this, hahmo, liikeVasemmalle);
+        Keyboard.Listen(Key.A, ButtonState.Up, Liiku, null, this, hahmo, Vector.Zero);
+        Keyboard.Listen(Key.D, ButtonState.Down, Liiku, "Hahmo liikkuu oikealle", this, hahmo, liikeOikealle);
+        Keyboard.Listen(Key.D, ButtonState.Up, Liiku, null, this, hahmo, Vector.Zero);
+    }
 
+
+    public static void Liiku(PhysicsGame peli, PhysicsObject hahmo, Vector nopeus)
+    {
+        hahmo.Velocity = nopeus;
+    }
+
+
+    
+    
 }
